dd2accaff70edd207f35fe626a4bff90c33bd49f
Brayan Mesquita <60306164+brayan-mesquita@users.noreply.github.com>
2022-10-27 21:00:13 -0400

    MacBook-Air-de-brayan.local em 27/10/2022 21:00

--- web_scraping/diof_ro.py
+++ web_scraping/diof_ro.py
@@ -27,6 +27,7 @@ links_pdf[0]
 
 
 #========ABRINDO ARQUIVOS======#
+import PyPDF2
 file_url = links_pdf[0]
 file = f'{links_pdf[0][-14:-4]}.pdf'
 request.urlretrieve(file_url , file )
@@ -34,40 +35,50 @@ arquivo_pdf = open('27.10.2022.pdf', 'rb')
 pdf = PyPDF2.PdfFileReader(arquivo_pdf)
 numero_paginas = pdf.getNumPages()
 numero_paginas
+
+#-----LEITURA DIOF
+
+arquivo_pdf = open('27.10.2022.pdf', 'rb')
+pdf = PyPDF2.PdfFileReader(arquivo_pdf)
+numero_paginas = pdf.getNumPages()
+paginas = pdf.getPage(130)
+paginas.extract_text()
+
+
+
+
+
 #===========GERENCIAR HORAS E DATA==============#
 #data_e_hora_atuais = datetime.now()
 #data_e_hora_atuais = data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')
 
 #===========CRIAR DIRETORIOS==============#
+from pathlib import Path
 p = Path()
-
+p.absolute()
+web_scraping = p / 'web_scraping'
+sub_pasta = web_scraping / 'sub_pasta'
+sub_pasta.mkdir()
 #transformando data, splitando separadores
 data = '10.10.2022'
 nova_pasta = p / '-'.join(data.split('.'))
-
 #criando pasta e arquivos
-for i in range(2):
-    pasta = p / f'pasta_numer_{i}'
-    pasta.mkdir()
-    arquivo = pasta / f'arquivo{i}.txt'
-    arquivo.touch()
-    with arquivo.open('a+') as text_file:
-        text_file.write(f'Arquivo numero{i}')
+lista_de_datas = ['10-10-2022', '11-10-2022']
+def criar_diretorios_e_arquivos(a_criar):
+    for name in a_criar:
+        pasta = p / 'web_scraping'/ 'sub_pasta' / f'{name}'
+        pasta.mkdir()
+        arquivo = pasta / f'{name}.txt'
+        arquivo.touch()
+        with arquivo.open('a+') as text_file:
+            text_file.write(f'Arquivo nome: {name}')
+#criar_diretorios_e_arquivos(lista_de_datas)
 
 def criar_diretorios():
     for pasta in links_pdf:
         dia = pasta[0][-14:-4]
         nova_pasta = path / dia
         nova_pasta.mkdir()
-
-criar_diretorios()
-
-type(dia)
-
-
-
-
-
 
 #===========DONWLOAD==============#
 from urllib import request
